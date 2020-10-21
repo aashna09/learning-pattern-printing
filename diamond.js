173f01d1472a3b74a1c398f2bf72f7acd96c99e8
Aashna Singh <saashna@gmail.com>
2020-10-21 18:44:38 +0530

    bugs removed in hollow diamond

--- diamond.js
+++ diamond.js
@@ -1,33 +1,41 @@
-number="";
-n=7;
-m=Math.ceil(n/2)
-space="";
-lspace=""
-lnumber=""
-temp=-1
-temp1=11
-for(i=1;i<=m;i++){
-  for(j=m-1;j>=i;j--){
-    space+="11"
-  }
-temp+=4
-  for(k=m;k<=temp-1;k++){
-number+="-"
-  }
- 
-  space+="*"+number+"*"+"\n"
-  
-  number=""
-}
-for(i=m-1;i>=1;i--){
-  for(j=i;j<=m-1;j++){
-  lspace+="11";
-  }
-  temp1+=-4
-   for(k=temp1;k>=1;k--){
-    lnumber+="-"
-  }
-lspace+="*"+lnumber+"*"+"\n"
-  lnumber=""
-}
-console.log(space+lspace)
+   // Adding starconst diamond = function(n){
+    var m = Math.ceil(n/2)
+    var no_of_spaces_left = (n-m)*2
+    var no_of_spaces_right = -1
+    for(var i = 1; i <= n; i++){
+      var line = ""
+      // Let's print spaces to the left
+      for(var j = 1; j <= no_of_spaces_left; j++){
+        line += " "
+      }
+    after spaces to the left
+      line += "*"
+      // Print double stars for all lines except 1st & last line
+      if(i != 1 && i != n){
+        // Increase if upper traingle
+        if(i <= m){
+          no_of_spaces_right += 4
+        }
+        // Decrease if lower traingle
+        else{
+          no_of_spaces_right -= 4
+        }
+        for(var j = 1; j <= no_of_spaces_right; j++){
+          line += " "
+        }
+        line += "*"
+      }
+      console.log(line)
+      // Decrease spaces to the left if i <= m
+      if(i < m){
+        no_of_spaces_left -= 2
+      }
+      // Increase spaces to the left if i > m
+      else{
+        no_of_spaces_left += 2
+      }
+    }
+   }
+    
+   diamond(11)
+   
