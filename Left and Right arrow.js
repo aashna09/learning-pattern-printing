bf06cb8c1ce739911c1274c9603528bf3df68555
Aashna Singh <saashna@gmail.com>
2020-10-21 19:44:27 +0530

    added left arrow

--- Left and Right arrow.js
+++ Left and Right arrow.js
@@ -1,27 +1,27 @@
 const diamond = function(n){
-    var m = Math.ceil(n/2)
-    var no_of_spaces_left = (n-m)*2
-    var no_of_spaces_right = -1
-    for(var i = 1; i <= n; i++){
-      var line = ""
-        for(var j = 0; j < no_of_spaces_left; j++){
-          if(i==m && j%2==0 && i<n){
-            line+="*"
-          }
-          else{
-           line += " "
-          }
+  var m = Math.ceil(n/2)
+  var no_of_spaces_left = (n-m)*2
+  var no_of_spaces_right = -1
+  for(var i = 1; i <= n; i++){
+    var line = ""
+      for(var j = 0; j < no_of_spaces_left; j++){
+        if(i==m && j%2==0 && i<n){
+          line+="*"
         }
-           line += "*"
-      console.log(line)
-      // Increase spaces to the left if i <= m
-      if(i < m){
-        no_of_spaces_left += 2
+        else{
+         line += " "
+        }
       }
-      // Decrease spaces to the left if i > m
-      else{
-        no_of_spaces_left -= 2
-      }
+         line += "*"
+    console.log(line)
+    // Increase spaces to the left if i <= m
+    if(i < m){
+      no_of_spaces_left += 2
     }
-   }
-   diamond(9)
+    // Decrease spaces to the left if i > m
+    else{
+      no_of_spaces_left -= 2
+    }
+  }
+ }
+ diamond(9)
